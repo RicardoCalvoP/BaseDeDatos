b07de5dce0f5acbc7dd4b5ce9758d380f349d528
RicardoCalvoP <a01028889@tec.mx>
2023-10-09 16:43:18 -0600

    .

--- Tarea5.h
+++ Tarea5.h
@@ -19,6 +19,7 @@ private:
     nodo *fin;
 
 public:
+    // Codigo visto en clase
     ListaDobleLigada();
     ~ListaDobleLigada();
     int ListaVacia();
@@ -27,6 +28,10 @@ public:
     void EliminaLista(int numero);
     void MostrarElementosInicio();
     void MostrarElementosFin();
+
+    // Funciones Tarea 5
+    void InsertarInicioLista(int numero);
+    void search(int numero);
 };
 
 ListaDobleLigada::ListaDobleLigada()
@@ -83,11 +88,10 @@ void ListaDobleLigada::InsertarFinalLista(int numero)
     }
     else
     {
-        inicio->anterior = elemento;
         elemento->valor = numero;
-        elemento->siguiente = inicio;
-        elemento->anterior = NULL;
-        inicio = elemento;
+        elemento->siguiente = NULL;
+        elemento->anterior = fin;
+        fin = elemento;
     }
 }
 
@@ -165,4 +169,68 @@ void ListaDobleLigada::MostrarElementosFin()
     cout << "Elemento [" << i << "] " << temporal->valor << "\n";
     temporal = temporal->anterior;
     i -= 1;
+}
+
+/*********FUNCIONES TAREA 5*************/
+
+void ListaDobleLigada::InsertarInicioLista(int numero)
+{
+    cout << "Agregando " << numero << " al Inicio de la lista\n";
+    nodo *elemento = new nodo();
+    if (elemento == NULL)
+    {
+        cout << "No se puede crear un elemento en la lista";
+        return;
+    }
+    if (ListaVacia() == 1)
+    {
+        elemento->valor = numero;
+        elemento->anterior = NULL;
+        elemento->siguiente = NULL;
+        inicio = elemento;
+        fin = elemento;
+    }
+    else
+    {
+        inicio->anterior = elemento;
+        elemento->valor = numero;
+        elemento->siguiente = inicio;
+        elemento->anterior = NULL;
+        inicio = elemento;
+    }
+}
+
+void ListaDobleLigada::search(int numero)
+{
+    int count = 0, posision = 0, j = 0;
+    int Position_Array[LongitudLista()];
+    nodo *temporal;
+    temporal = inicio;
+    while (temporal != NULL)
+    {
+        if (temporal->valor == numero)
+        {
+            Position_Array[count] = posision;
+            count += 1;
+        }
+        temporal = temporal->siguiente;
+        posision += 1;
+    }
+    if (count == 0)
+    {
+        cout << "El numero " << numero << " no esta en la lista\n";
+    }
+    else if (count == 1)
+    {
+        cout << "El numero " << numero << " fue encotrado " << count << " vez en la posision " << Position_Array[0] << "\n";
+    }
+    else
+    {
+        cout << "El numero " << numero << "fue encotrado " << count << " veces en las posiciones ";
+        for (int i = 0; i < count - 1; i++)
+        {
+            cout << Position_Array[i] << ", ";
+        }
+        cout << posision << "\n";
+    }
 }
