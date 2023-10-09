87cad5e52a4582f1b47e0f65d6400c072668de1a
RicardoCalvoP <a01028889@tec.mx>
2023-10-09 11:21:12 -0600

    Lista Doblemente Ligada

--- Tarea5.h
+++ Tarea5.h
@@ -1,4 +1,4 @@
-#include <istream>
+#include <iostream>
 using namespace std;
 
 class nodo
@@ -17,10 +17,74 @@ private:
 
 public:
     ListaDobleLigada();
+    // Consultar si la lista no tiene algun nodo integrado
+    bool ListaVacia();
+    // Consultar la longitud de la lista
+    int LongitudLista();
+    // Insertar Elementos al Final de la lista
+    void InsertaFinalLista(int num);
+    // Insertar Elementos al Inicio de la lista
+    void InsertaInicioLista(int num);
 };
 
 ListaDobleLigada::ListaDobleLigada()
 {
     inicio = NULL;
     fin = NULL;
+}
+
+inline bool ListaDobleLigada::ListaVacia()
+{
+    if (inicio == NULL && fin == NULL)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int ListaDobleLigada::LongitudLista()
+{
+    nodo *temporal;
+    int longitud;
+    temporal = inicio;
+    while (temporal != NULL)
+    {
+        longitud += 1;
+        temporal = temporal->siguiente;
+    }
+    return longitud;
+}
+
+void ListaDobleLigada::InsertaFinalLista(int num)
+{
+    cout << "Nuevo eleento en el final de la lista: " << num << "\n";
+    nodo *elemento = new nodo();
+    if (elemento == NULL)
+    {
+        cout << "No se puede crear un elemento en la lista\n";
+        return;
+    }
+    if (ListaVacia() == true)
+    {
+        elemento->valor = num;
+        elemento->anterior = NULL;
+        elemento->siguiente = NULL;
+        inicio = elemento;
+        fin = elemento;
+    }
+    else
+    {
+        elemento->valor = num;
+        elemento->siguiente = NULL;
+        elemento->anterior = fin;
+        fin->siguiente = elemento;
+        fin = elemento;
+    }
+}
+
+void ListaDobleLigada::InsertaInicioLista(int num)
+{
 }
