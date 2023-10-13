cab30a8cc2dac48391fec4b4c6c77623e6586af7
RicardoCalvoP <a01028889@tec.mx>
2023-10-13 11:42:46 -0600

    Main

--- Main.cpp
+++ Main.cpp
@@ -12,10 +12,15 @@ int main()
     LDL.InsertarFinalLista(4);
     LDL.InsertarFinalLista(3);
     LDL.InsertarFinalLista(2);
+    cout << "\n";
     LDL.MostrarElementosInicio();
-
+    cout << "\n";
+    LDL.Promedio();
+    cout << "\n";
     LDL.EliminarPares();
+    cout << "\n";
     LDL.MostrarElementosInicio();
-
+    cout << "\n";
+    LDL.Promedio();
     return 0;
 }
