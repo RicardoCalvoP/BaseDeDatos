91e059762f815f41735b9f3df20f1cdcd2e928a7
RicardoCalvoP <a01028889@tec.mx>
2023-10-13 19:31:17 -0600

    Main

--- Main.cpp
+++ Main.cpp
@@ -9,6 +9,14 @@ int main()
     LL.InsertarInicioLista(3);
     LL.InsertarInicioLista(2);
     LL.InsertarInicioLista(1);
+    LL.InsertarInicioLista(3);
+    LL.InsertarInicioLista(6);
+    LL.InsertarInicioLista(1);
+    LL.InsertarInicioLista(3);
+    LL.InsertarInicioLista(10);
+    LL.InsertarInicioLista(4);
+    LL.InsertarInicioLista(1);
+    LL.EliminaNumeroLista(3);
     LL.Show();
     return 0;
 }
