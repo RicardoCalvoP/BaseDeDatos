bf611f27eddfc72a4453d5d9fa5d4cef7433c08a
RicardoCalvoP <a01028889@tec.mx>
2023-11-24 21:47:44 -0600

    Continuar

--- Proyecto_Final/FuerzaBruta.py
+++ Proyecto_Final/FuerzaBruta.py
@@ -1,34 +1,30 @@
-import os
+import random as random
+import time as time
 import string
-
-
-def imprimir_tablero(tablero):
-    for fila in tablero:
-        print(" ".join(str(casilla) for casilla in fila))
-
+import os
 
 def crear_tablero(size):
     tablero = []
     # Crea la lista de letras de 'a' a 'z'
     letras = [' '] + list(string.ascii_lowercase)[:size]
     if (size < 27):
-        for i in range(size + 1):
-            fila = []
-            for j in range(size + 1):
-                if (j == 0):
-                    if (i == 0):
-                        fila.append("  ")
+        for fila in range(size + 1):
+            linea = []
+            for columna in range(size + 1):
+                if (columna == 0):
+                    if (fila == 0):
+                        linea.append("  ")
                     else:
-                        if ((size + 1 - i) < 10):
-                            fila.append(str(size + 1 - i) + " ")
+                        if ((size + 1 - fila) < 10):
+                            linea.append("0"+str(size + 1 - fila))
                         else:
-                            fila.append(size + 1 - i)
-                elif (i == 0):
-                    fila.append(letras[j])
+                            linea.append(size + 1 - fila)
+                elif (fila == 0):
+                    linea.append(letras[columna])
                 else:
-                    fila.append("o")
-            tablero.append(fila)
-        print(f"Tablero de {size} x {size} correctamente")
+                    linea.append("o")
+            tablero.append(linea)
+        print(f"Tablero de {size} x {size} creado correctamente\n")
 
         imprimir_tablero(tablero)
         return tablero
@@ -36,10 +32,15 @@ def crear_tablero(size):
         print("No se ha podido crear el tablero")
         print("Intente con un tablero con tamaño menoe a 27")
 
+def seguro(tablero, fila, columna, size):
+    for i in range(fila):
+        if tablero[i][columna] == 'Q':
+            return False
 
-def validar_Posicion(tablero):
-    print(tablero)
+def imprimir_tablero(tablero):
+    for linea in tablero:
+        print(" ".join(str(casilla) for casilla in linea))
 
 
 os.system("cls")
-crear_tablero(20)
+crear_tablero(8)
---
+++ Proyecto_Final/Pruebas.py
@@ -0,0 +1,50 @@
+import random as random
+import string
+import os
+
+def resolverNreinas(size):
+    tablero = crear_tablero(size)
+    num_soluciones = 0
+    colocar_reinas(tablero, 0, size, num_soluciones)
+    print(f"Se encontraron {num_soluciones} soluciones para {size} reinas.")
+
+def crear_tablero(size):
+    tablero = [['o' for _ in range(size)] for _ in range(size)]
+    return tablero
+
+def imprimir_tablero(tablero):
+    for fila in tablero:
+        print(' '.join(fila))
+
+def es_seguro(tablero, fila, columna, size):
+    for i in range(fila):
+        if tablero[i][columna] == 'Q':
+            return False
+
+        for j in range(columna, size):
+            if tablero[i][j] == 'Q' and i + j == fila + columna:
+                return False
+
+            if tablero[i][j] == 'Q' and i - j == fila - columna:
+                return False
+
+            if tablero[i][j] == 'Q' and i - j == fila - columna:
+                return False
+
+    return True
+
+def colocar_reinas(tablero, fila, size, num_soluciones):
+    if fila == size:
+        num_soluciones = num_soluciones +1
+        print(f"Solución #{num_soluciones}")
+        imprimir_tablero(tablero)
+        return num_soluciones
+
+    for columna in range(size):
+        if es_seguro(tablero, fila, columna, size):
+            tablero[fila][columna] = 'Q'
+            colocar_reinas(tablero, fila + 1, size, num_soluciones)
+            tablero[fila][columna] = 'o'
+
+os.system("cls")
+resolverNreinas(8)  # Cambia el número de reinas aquí
